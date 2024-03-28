881d18f28908d83649a581479c2c39f60eaec966
pavan kumar <rxpavan@gmail.com>
2024-03-28 14:50:59 +0530

    Update ToGetAGORA_TICS

--- PaperThermOptiCS/ToGetAGORA_TICs.m
+++ PaperThermOptiCS/ToGetAGORA_TICs.m
@@ -1,15 +1,17 @@
+% This code enumerates on all the AGORA2 models and gets the TICs and
+% their respective directions.
 clear
 % initCobraToolbox(0);
-p = dir('../../Microbial_community_gap_filling/With_AGORA2/AGORA2/');
+p = dir('./AGORA2/'); % path to agora models
 agoraModels = {p(3:end).name}';
 p2 = dir('./AGORA_TIC_Results/');
 p2 = {p2(3:end).name}';
 agoraModels = setdiff(agoraModels,p2);
-agoraModels = agoraModels(randperm(length(agoraModels)));
+
 for i =1:numel(agoraModels)
-    load(['../../Microbial_community_gap_filling/With_AGORA2/AGORA2/',agoraModels{i}])
+    load(['./AGORA2/',agoraModels{i}])
     
-    [TICs,Direction,TIC_Rxns,modModel,opt] = ThermoOptEnumMILP(model,300);
+    [TICs,Direction,TIC_Rxns,modModel,opt] = ThermOptEnumMILP(model,1800);
     if opt
         save(['./AGORA_TIC_Results/',agoraModels{i}],'TICs','Direction','TIC_Rxns')
     end
